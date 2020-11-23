be2b054e3e3fead0baa511685742700b41ebfd00
+ <mcrea23@aol.com>
2020-11-23 17:22:41 -0600

    commit tree 0eebd7e4e4c42ee0787b5dfea0c5f131ceb32f06

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,33 @@
 class BankAccount
 
+attr_reader :name
+attr_accessor :balance, :status
+
+def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+end
+
+def deposit(amount)
+    self.balance += amount
+end
+
+def display_balance
+    "Your balance is $#{balance}."
+end
+
+def close_account
+    self.status = "closed"
+end
+
+def valid?
+    if @balance <= 0 
+        false
+    elsif @status == "closed"
+        false
+    else
+        true
+end
+end
 end
